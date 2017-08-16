5009b379262a52e0678d20bd10b8b2ac4d9dca28
d-kang <david.kang714@gmail.com>
2017-08-15 18:36:56 -0700

    Clear Refactored Code

--- seed.js
+++ seed.js
@@ -1,64 +1,9 @@
-var userData = require('./exampleUsers.json');
-var templateData = require('./exampleTemplate.json');
-var activityData = require('./exampleActivity.json');
-var historyData = require('./exampleHistory.json');
-var model = require('./db/mongoose-schemas.js')
+var userData = require('./exampleUsers');
+var templateData = require('./exampleTemplate');
+var activityData = require('./exampleActivity');
+var historyData = require('./exampleHistory');
+var model = require('./db/mongoose-schemas')
 
-
-// var mongoose = require('mongoose');
-// var Schema = mongoose.Schema;
-// mongoose.connect('mongodb://localhost/workout', {
-//   useMongoClient: true
-// });
-
-//CREATE THE SCHEMA AND MODELS
-
-// var userSchema = new Schema({
-//   id: String,
-//   partner_user_id: String,
-//   name: String,
-//   email: String,
-//   goal: String
-// });
-//
-// var User = mongoose.model('User', userSchema);
-//
-// var templateSchema = new Schema({
-//   id: String,
-//   user_id: String,
-//   date: String,
-//   name: String,
-//   description: String
-// });
-//
-// var Template = mongoose.model('Template', templateSchema);
-//
-// var activitySchema = new Schema({
-//   id: String,
-//   user_id: String,
-//   workout_id: String,
-//   type: String,
-//   set_number: Number,
-//   repetition_number: Number,
-//   set_time: Number,
-//   rest_time: Number,
-//   timed: Boolean,
-//   name: String,
-//   description: String,
-//   date: Date
-// });
-//
-// var Activity = mongoose.model('Activity', activitySchema);
-//
-// var historySchema = new Schema({
-//   id: String,
-//   completed: Boolean,
-//   user_id: String,
-//   workout_id: String,
-//   date: Date
-// });
-//
-// var History = mongoose.model('History', historySchema);
 
 //MAKE SURE TABLES ARE CLEAR
 model.User.remove({}, function(err) {
@@ -95,36 +40,3 @@ activityData.forEach(function(element) {
 historyData.forEach(function(element) {
   model.History.create(element);
 });
-
-// fs.readFile('exampleUsers.json', 'utf8', (err, data) => {
-//   if (err) console.log(err);
-//   for (var i = 0; i < data.length; i++) {
-//     console.log(data[i]);
-//     User.create(data[i]);
-//   };
-// });
-
-/*fs.readFile('exampleTemplate.json', 'utf8', (err, data) => {
-  if (err) console.log(err);
-  for (var i = 0; i < data.length; i++) {
-    console.log(data[i]);
-    Template.create(data[i]);
-  };
-});
-
-fs.readFile('exampleActivity.json', 'utf8', (err, data) => {
-  if (err) console.log(err);
-  for (var i = 0; i < data.length; i++) {
-    console.log(data[i]);
-    Activity.create(data[i]);
-  };
-});
-
-fs.readFile('exampleHistory.json', 'utf8', (err, data) => {
-  if (err) console.log(err);
-  for (var i = 0; i < data.length; i++) {
-    console.log(data[i]);
-    History.create(data[i]);
-  };
-});
-*/
