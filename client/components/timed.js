1197c3859c9be81986be536be975b2f7c3872dae
Ankit09osr <ankit09osr@gmail.com>
2017-08-17 21:47:54 -0700

    Add addTemplate() which help send template of a user to the database

--- client/components/timed.js
+++ client/components/timed.js
@@ -1,12 +1,17 @@
 angular.module('sparrow')
 .controller('TimedCtrl', function() {
+   //workout contain template created by user.
   this.workout = [];
-  //workout  contain template created by user.
+  //addWorkout() create a template of named workout
   this.addWorkout = function() {
     var obj = {};
     obj.activity = this.activity;
     obj.duration = Number(this.minutes || 0) * 60 + Number(this.seconds);
     this.workout.push(obj);
+  };
+  //addTemplate add template to the database.
+  this.addTemplate = function() {
+    console.log('Send present workout to the database via http services',this.workout);
   };
 })
 .component('timed', {
