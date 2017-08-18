b62e4d5d56b6693ac79e18d0f7ddb992b0943e76
Nick <nhattwick@gmail.com>
2017-08-18 13:00:38 -0700

    Updated Seed.js

--- seed.js
+++ seed.js
@@ -2,7 +2,8 @@ const userData = require('./mockdata/exampleUsers');
 const templateData = require('./mockdata/exampleTemplate');
 const activityData = require('./mockdata/exampleActivity');
 const historyData = require('./mockdata/exampleHistory');
-const { User, Template, Activity, History } = require('./db/mongoose-schemas.js')
+const goalData = require('./mockdata/exampleGoal');
+const { User, Template, History, Goal } = require('./db/mongoose-schemas.js')
 
 
 //MAKE SURE TABLES ARE CLEAR
@@ -16,27 +17,38 @@ Template.remove({}, function(err) {
   console.log('Template Table Cleared');
 })
 
-Activity.remove({}, function(err) {
-  if (err) console.log(err);
-  console.log('Activity Table Cleared');
-})
+// Activity.remove({}, function(err) {
+//   if (err) console.log(err);
+//   console.log('Activity Table Cleared');
+// })
 
 History.remove({}, function(err) {
   if (err) console.log(err);
   console.log('History Table Cleared');
 })
 
+Goal.remove({}, function(err) {
+  if (err) console.log(err);
+  console.log('Goal Table Cleared');
+})
 //ADD DATA
 
 userData.forEach(function(element) {
   User.create(element);
+  console.log('User: ', element);
 });
 templateData.forEach(function(element) {
   Template.create(element);
+  console.log('Template: ', element);
 });
-activityData.forEach(function(element) {
-  Activity.create(element);
-});
+// activityData.forEach(function(element) {
+//   Activity.create(element);
+// });
 historyData.forEach(function(element) {
   History.create(element);
+  console.log('History: ', element);
+});
+goalData.forEach(function(element) {
+  Goal.create(element);
+  console.log('Goal: ', element);
 });
