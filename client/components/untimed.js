05fa2ed7a776bec0d6c1f1d9353613ebb53ccfa7
Ankit09osr <ankit09osr@gmail.com>
2017-08-17 21:56:51 -0700

    Add addWorkout() which help create a template of untimed workout

--- client/components/untimed.js
+++ client/components/untimed.js
@@ -1,6 +1,13 @@
 angular.module('sparrow')
 .controller('UnTimedCtrl', function() {
-
+  this.workout = [];
+  this.addWorkout = function() {
+    var obj = {};
+    obj.activity = this.activity;
+    obj.sets = this.sets;
+    obj.reps = this.reps;
+    this.workout.push(obj);
+  };
 })
 .component('untimed', {
   controller: 'UnTimedCtrl',
