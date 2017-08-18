0a39048ed5b9fe93d42addf380e7e2072ae7179f
Ankit09osr <ankit09osr@gmail.com>
2017-08-17 21:42:34 -0700

    Add addWorkout() which help create a template of workout

--- client/components/timed.js
+++ client/components/timed.js
@@ -1,6 +1,13 @@
 angular.module('sparrow')
 .controller('TimedCtrl', function() {
-
+  this.workout = [];
+  //workout  contain template created by user.
+  this.addWorkout = function() {
+    var obj = {};
+    obj.activity = this.activity;
+    obj.duration = Number(this.minutes || 0) * 60 + Number(this.seconds);
+    this.workout.push(obj);
+  };
 })
 .component('timed', {
   controller: 'TimedCtrl',
