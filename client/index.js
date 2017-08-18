150e668d452c2aeb937f8f2f213d241e52477a7b
Nicholas Orr <orr.nicholas.sean@gmail.com>
2017-08-18 02:01:05 +0200

    merged index.js index.html with Ankit + Nick changes

--- client/index.js
+++ client/index.js
@@ -23,13 +23,43 @@ angular.module('sparrow',['ui.router'])
     },
     {
       name:'goals',
-      url: '/goals',
-      component: 'goals'
+      url:'/goals',
+      component: 'goals',
+      resolve : {
+        resolveGoal : function (goalService) {
+          var goals=goalService.getAllGoals();
+          //console.log( goals);
+          return goals;
+        }
+      }
+    },
+    {
+      name: 'goal',
+      url: '/{goalID}',
+      parent:'goals',
+      component : 'goal',
+      resolve : {
+        resolveGoalItem : function (goalService,$transition$){
+          var goalDetailAfterUSerClick= goalService.getGoal($transition$.params().goalID)
+          console.log('in resolve goal, goalDetailAfterUSerClick = '+JSON.stringify(goalDetailAfterUSerClick));
+          return goalDetailAfterUSerClick;
+        }
+      }
+    },
+    {
+      name :'addGoal',
+      url:'/addGoal',
+      parent :'goals',
+      component : 'addGoal'
     }
   ];
    // Loop over the state definitions and register them
   states.forEach(function(state) {
     $stateProvider.state(state);
   });
-});
+});/*.run(function($http, $uiRouter) {
+  var Visualizer = window['ui-router-visualizer'].Visualizer;
+  $uiRouter.plugin(Visualizer);
+  $http.get('services/fakeJSONgoals.js', { cache: true });
+});*/
 
