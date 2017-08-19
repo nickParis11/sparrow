48b797582175e0569e6be10db92a589bcb074daf
d-kang <david.kang714@gmail.com>
2017-08-19 09:18:01 -0700

    Delete Original Components folder After Integration

--- client/components/workout.js
+++
@@ -1,8 +0,0 @@
-angular.module('sparrow')
-.controller('WorkoutCtrl', function WorkoutCtrl() {
-
-})
-.component('workout', {
-  controller: 'WorkoutCtrl',
-  templateUrl: 'templates/workout.html'
-});
