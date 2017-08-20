c740a6e0a8ba5783287d3e3ca11543041330cc46
Ankit09osr <ankit09osr@gmail.com>
2017-08-20 09:32:04 -0700

    Render timed and untimed based on condition

--- src/client/components/workout-component/workout.js
+++ src/client/components/workout-component/workout.js
@@ -5,12 +5,18 @@ angular.module('sparrowFit')
   this.workoutData = '';
   this.workout = '';
   this.user_id = 'xyz';
-  this.timed = false;
   this.url = '/api/get/workout/'+this.user_id;
   this.name = function () {
     console.log('This is the input Name :',this.inputName);
     this.workoutData = userDataService.getWorkout(this.userData, this.inputName);//jogging,a
     this.workout = this.workoutData.template;
+    this.timed = this.workoutData.timed;
+    if(this.timed===false){
+      this.untimed = true;
+    } else {
+      this.untimed = false;
+    }
+    console.log('Timed value :', this.timed);
   };
 
 
