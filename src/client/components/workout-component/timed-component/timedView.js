abbcf71903129ba5eb0b4db09cbf40d0bd113299
Ankit09osr <ankit09osr@gmail.com>
2017-08-20 19:34:59 -0700

    Code clean up for loading timer

--- src/client/components/workout-component/timed-component/timedView.js
+++ src/client/components/workout-component/timed-component/timedView.js
@@ -1,25 +1,20 @@
 angular.module('sparrowFit')
 .controller('timedViewCtrl', function(httpService, timerService) {
   this.timer = timerService;
-
-   // var testData = [['Dance', 5], ['Break', 5], ['Crunches', 6], ['Break', 5], ['Planks', 4]];
   var testData = [];
 
-  console.log('workout binding: ', this.workout);
-  console.log('timedView timerd: ', this.timerd);
-  // this.timer.clock(testData);
-  this.get = function () {
-    console.log('Hello');
-    console.log("Workout: ", this.workout);
+
+  this.loadData = function () {
     for (var i = 0; i < this.workout.length; i++) {
       var tuple = [this.workout[i].activity, this.workout[i].duration];
       testData.push(tuple);
       testData.push(['Break', this.workout[i].break]);
     }
-    console.log('Test Data: ', testData);
-    this.timer.clock(testData);
 
+    this.timer.clock(testData);
   };
+
+      // this.timer.clock(testData);
   this.msg = 'Timer is working';
   console.log('This is message :',this.msg);
   this.completed = () => {
