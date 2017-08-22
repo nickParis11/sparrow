f120511ca2bca1c99dc7901e1e94e9a66758abfd
d-kang <david.kang714@gmail.com>
2017-08-21 19:44:40 -0700

    Deconsole Index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/config', (req, res) => {
-  console.log('/client==========================')
   var data = {
     domain: process.env.DOMAIN,
     clientID: process.env.CLIENT_ID
@@ -39,7 +38,6 @@ app.get('/api/get/application', (req, res) => {
 const authCheck = jwt({ secret: process.env.SECRET, audience: process.env.AUDIENCE, credentialsRequired: true });
 // SET UP A PUBLIC AND PRIVATE ENDPOINT
 app.get('/api/public', (req, res) => {
-  console.log('hi from /api/public')
   res.json({ message: "Hello from a public endpoint! You don't need to be authenticated to see this." })
 });
 
@@ -108,25 +106,21 @@ app.get('/api/get/histories', function(req, res) {
 //HANDLE POST REQUESTS
 
 app.post('/api/post/users', function(req, res) {
-  console.log(req.body);
   User.create(req.body);
   res.send('Posted User');
 });
 
 app.post('/api/post/workout', function(req, res) {
-  console.log(req.body);
   Template.create(req.body);
   res.send('Posted Template');
 });
 
 app.post('/post/goals', function(req, res) {
-  console.log(req.body);
   Goal.create(req.body);
   res.send('Posted Goal');
 });
 
 app.post('/api/post/histories', function(req, res) {
-  console.log(req.body);
   History.create(req.body);
   res.send('Posted History');
 });
@@ -137,14 +131,12 @@ app.post('/api/post/histories', function(req, res) {
 //GET USER BY USER_ID
 app.get('/api/get/users/:id', function(req, res) {
   var ident = req.params.id;
-  console.log(ident);
   var user = null;
   User.find({id: ident }, function(err, target) {
     if (err) console.log(err);
     user = target;
   })
   .then(function() {
-    console.log('user: ', user);
     res.send(user);
   });
 });
@@ -159,7 +151,6 @@ app.get('/api/get/workout/:user', function(req, res) {
     templates.push(template);
   })
   .then(function() {
-    console.log(templates);
     res.send(templates);
   });
 });
@@ -174,7 +165,6 @@ app.get('/api/get/histories/:user', function(req, res) {
     histories.push(history);
   })
   .then(function() {
-    console.log(histories);
     res.send(histories);
   });
 });
@@ -188,7 +178,6 @@ app.get('/api/get/goals/:user', function(req, res) {
     goals.push(goal);
   })
   .then(function() {
-    console.log(goals);
     res.send(goals);
   });
 });
@@ -198,3 +187,4 @@ app.get('/api/get/goals/:user', function(req, res) {
 app.listen(PORT, function() {
   console.log(`Node app is running on http://localhost:${PORT}`);
 });
+console.log();
